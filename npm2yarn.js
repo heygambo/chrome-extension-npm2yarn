00c4e6297462bf86b2f8cffaa9fef222d8ed306c
Gunther Schulz <g.schulz@atenekom.eu>
2018-11-20 13:04:28 +0100

    Sdd global options
    Replaces npm's -g with yarn's global

--- npm2yarn.js
+++ npm2yarn.js
@@ -1,4 +1,5 @@
 var $codeTags = document.querySelectorAll('code > span')
 $codeTags.forEach(function ($codeTag) {
   $codeTag.textContent = $codeTag.textContent.replace('npm i', 'yarn add')
+  $codeTag.textContent = $codeTag.textContent.replace('npm i -g', 'yarn global add')
 })
